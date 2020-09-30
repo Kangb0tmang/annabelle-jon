d882e2eb81e628cfcc075cc38e77cfa5025409af
Kangb0tmang <Jon.Kang.JD@gmail.com>
2020-09-30 18:03:01 +1000

    React helmet, rename layout to reset

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { Box } from 'rebass';
 import { ThemeProvider } from 'emotion-theming';
 import { theme } from '../styles/theme';
-import '../styles/layout.scss';
+import '../styles/reset.scss';
 
 const Layout = ({ children }) => {
   return (
     <ThemeProvider theme={theme}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="robots" content="noindex" />
+        <title>Annabelle Jon</title>
+      </Helmet>
       <Box sx={{ position: 'relative' }}>{children}</Box>
     </ThemeProvider>
   );
