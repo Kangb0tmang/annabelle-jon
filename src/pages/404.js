106a72e142ff7f7eb196701f7b700340ef09c3eb
Kangb0tmang <Jon.Kang.JD@gmail.com>
2021-01-13 17:03:42 +1100

    Basic 404 page styles update

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { Box, Text } from 'rebass';
+import PageHeading from '../components/PageHeading';
 import Page from '../components/Page';
 import SEO from '../components/SEO';
-import PageHeading from '../components/PageHeading';
+import { theme } from '../styles/theme';
 
 const NotFoundPage = () => (
   <Page>
-    <SEO title="404: Not found" />
-    <PageHeading>Page not found</PageHeading>
-    <Link to="/home">Head back home</Link>
+    <SEO title="Page Not Found" />
+    <Box sx={{ my: ['50px', '', '100px'] }}>
+      <PageHeading>Page not found</PageHeading>
+      <Text
+        as={Link}
+        to="/"
+        sx={{
+          color: theme.colours.black,
+          '&:hover': {
+            color: theme.colours.black,
+          },
+          '&:focus': {
+            color: theme.colours.black,
+          },
+        }}
+      >
+        Back home
+      </Text>
+    </Box>
   </Page>
 );
 
