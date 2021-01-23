e415d72365c889e17bf85ef955eadfa20bdddb0d
Kangb0tmang <Jon.Kang.JD@gmail.com>
2021-01-23 17:20:36 +1100

    InfoWindow styles, custom marker

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,18 +1,21 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Box } from 'rebass';
+import React, { useState } from 'react';
+import { Box, Text } from 'rebass';
 import {
   GoogleMap,
   LoadScript,
   Marker,
   InfoWindow,
 } from '@react-google-maps/api';
+import { theme } from '../styles/theme';
+import marker from '../images/marker.png';
 
 // https://medium.com/@allynak/how-to-use-google-map-api-in-react-app-edb59f64ac9d
 // https://tomchentw.github.io/react-google-maps/#introduction
 
 const venue = {
-  title: "The Wedding Venue: Potter's Receptions",
-  location: { lat: -37.74678130874577, lng: 145.23648631767588 },
+  address:
+    '<h2>Potters Receptions</h2><p>321 Jumping Creek Rd</p><p>Warrandyte VIC 3113</p><a href="https://maps.google.com/maps?ll=-37.746879,145.236478&z=15&t=m&hl=en-GB&gl=US&mapclient=apiv3&cid=5984047495981433388" target="_blank">View on Google Maps</a>',
+  location: { lat: -37.747048065011704, lng: 145.23653060432648 },
 };
 
 const Map = () => {
@@ -37,12 +40,14 @@ const Map = () => {
       >
         <GoogleMap
           mapContainerStyle={mapStyles}
-          zoom={16}
+          zoom={15}
           center={venue.location}
+          clickableIcons={false}
         >
           <Marker
             key={'My Marker'}
             position={venue.location}
+            icon={marker}
             onClick={() => onSelect(venue)}
           />
           {selected.location && (
@@ -51,7 +56,35 @@ const Map = () => {
               clickable={true}
               onCloseClick={() => setSelected({})}
             >
-              <p>{selected.title}</p>
+              <Text
+                sx={{
+                  h2: {
+                    mb: '8px',
+                    fontSize: ['15px', '', '17px'],
+                  },
+                  p: {
+                    lineHeight: '16px',
+                    '&:last-of-type': {
+                      mb: '5px',
+                    },
+                  },
+                  a: {
+                    mt: '5px',
+                    lineHeight: '16px',
+                    textDecoration: 'none',
+                    color: theme.colours.lightblue,
+                    '&:hover': {
+                      textDecoration: 'underline',
+                      color: theme.colours.navy,
+                    },
+                    '&:focus': {
+                      textDecoration: 'underline',
+                      color: theme.colours.navy,
+                    },
+                  },
+                }}
+                dangerouslySetInnerHTML={{ __html: selected.address }}
+              />
             </InfoWindow>
           )}
         </GoogleMap>
