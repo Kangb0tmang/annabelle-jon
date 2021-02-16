948ccde52670ee2f5a6b1020b8a90de819711c93
Kangb0tmang <Jon.Kang.JD@gmail.com>
2021-02-16 21:29:20 +1100

    Gatsby file cleanup, edit siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
   siteMetadata: {
     siteUrl: 'https://thekangs.wedding',
     title: `Annabelle & Jon`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `RSVP site for Annabelle and Jon's wedding`,
     author: `@gatsbyjs`,
   },
   plugins: [
