853e808490927ed2cb7a52d74b174e15547127cf
Kangb0tmang <Jon.Kang.JD@gmail.com>
2021-07-22 22:08:11 +1000

    FAQ content change

--- src/content/faq-content.js
+++ src/content/faq-content.js
@@ -1,7 +1,7 @@
 export const faqs = [
   {
     question: 'When is the RSVP deadline?',
-    response: 'Please respond by Friday 2nd of April 2021.',
+    response: 'Please respond by Monday 26 of July 2021.',
   },
   {
     question: 'Can I bring a plus one?',
